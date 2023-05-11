257ac152ce61754a823b16156a7ca9dfc2653948
Lida Nil <lidanil196@gmail.com>
2023-05-11 15:04:39 +0700

    Add project files.

---
+++ Diskon/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(129, 72)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(127, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Jumlah Barang"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(129, 379)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Total Bayar"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(129, 133)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(120, 25)
+        Label3.TabIndex = 1
+        Label3.Text = "Harga Barang"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(129, 259)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(101, 25)
+        Label4.TabIndex = 2
+        Label4.Text = "Keterangan"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(129, 190)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(102, 25)
+        Label5.TabIndex = 1
+        Label5.Text = "Total Harga"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(129, 323)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(67, 25)
+        Label6.TabIndex = 3
+        Label6.Text = "Diskon"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(371, 72)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(371, 133)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Enabled = False
+        TextBox3.Location = New Point(371, 190)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Enabled = False
+        TextBox4.Location = New Point(371, 259)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(150, 31)
+        TextBox4.TabIndex = 7
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Enabled = False
+        TextBox5.Location = New Point(371, 323)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(150, 31)
+        TextBox5.TabIndex = 8
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Enabled = False
+        TextBox6.Location = New Point(371, 379)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(150, 31)
+        TextBox6.TabIndex = 9
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(720, 72)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 62)
+        Button1.TabIndex = 10
+        Button1.Text = "Hitung Total Harga"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(720, 148)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 67)
+        Button2.TabIndex = 11
+        Button2.Text = "Tampilkan Keterangan"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(720, 241)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(112, 60)
+        Button3.TabIndex = 12
+        Button3.Text = "Hitung Diskon"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(720, 321)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 62)
+        Button4.TabIndex = 13
+        Button4.Text = "Hitung Total Bayar"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.R__1_
+        ClientSize = New Size(989, 522)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label6)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label5)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Diskon/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox3.Text >= 2000000 Then
+            TextBox4.Text = "Diskon 20%"
+        ElseIf TextBox3.Text >= 1000000 Then
+            TextBox4.Text = "Diskon 10%"
+        Else
+            TextBox4.Text = "Tidak Ada Diskon"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox4.Text = "Diskon 10%" Then
+            TextBox5.Text = Val(TextBox3.Text) * 10 / 100
+        ElseIf TextBox4.Text = "Diskon 20%" Then
+            TextBox5.Text = Val(TextBox3.Text) * 20 / 100
+        Else
+            TextBox5.Text = "0"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox6.Text = Val(TextBox3.Text) - Val(TextBox5.Text)
+    End Sub
+End Class
